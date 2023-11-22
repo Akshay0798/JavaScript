25900dcd9830f28e90a2b52f6f6bde15c72658e9
Akshay Umredkar <akshayumredkar9822@gmail.com>
2023-11-22 17:42:23 +0530

    update

--- variableAndDatatype.js
+++ variableAndDatatype.js
@@ -1,11 +1,41 @@
-fullName = "Akshay Umredkar";
-console.log(fullName);
+// fullName = "Akshay Umredkar";
+// console.log(fullName);
 
-age=26;
-console.log(age);
+// age=26;
+// console.log(age);
 
-x=null; //i know what is it but its empty value
-console.log(x);
+// x=null; //i know what is it but its empty value
+// console.log(x);
 
-y=undefined; //undefine means pata hi nahi esme kya he
-console.log(y);
+// y=undefined; //undefine means pata hi nahi esme kya he
+// console.log(y);
+
+// let x=BigInt('123');
+
+// const student={
+//     fullName : "Akshay",
+//     age:20,
+//     cgpa:8.9,
+//     isPass:true
+// };
+
+// student['age'] = student['age'] + 6;
+// console.log(student.age);
+
+// const product = {
+//     title:"ball Pen",
+//     rating:4,
+//     offer:5,
+//     price:270
+// };
+// console.log(product);
+
+const profile = {
+    username:"@shraddhaKK",
+    name: "shraddha Khapra",
+    isFollow:true,
+    message:"Thank you",
+    posts:195,
+    follower:600,
+    Bio:"Ex-Microsoft Employ",
+};
