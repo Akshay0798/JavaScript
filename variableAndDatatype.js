02ae374c7464ebb7c75c34c3a3d2e1e2009337e4
Akshay Umredkar <akshayumredkar9822@gmail.com>
2023-11-22 17:01:14 +0530

    creat variable and datatype file

---
+++ variableAndDatatype.js
@@ -0,0 +1,11 @@
+fullName = "Akshay Umredkar";
+console.log(fullName);
+
+age=26;
+console.log(age);
+
+x=null; //i know what is it but its empty value
+console.log(x);
+
+y=undefined; //undefine means pata hi nahi esme kya he
+console.log(y);
