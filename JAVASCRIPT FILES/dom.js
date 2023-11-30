5cc3de6342ab3553a1250c3c4a9175c1700b4770
Akshay Umredkar <akshayumredkar9822@gmail.com>
2023-11-30 18:45:03 +0530

    create file

---
+++ JAVASCRIPT FILES/dom.js
@@ -0,0 +1,24 @@
+//? DOM Manipulation
+
+//! selection with id
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+
+//! selecting with class
+// let cls = document.getElementsByClassName("cls");
+// console.dir(cls);
+
+// ! selecting with tag 'p'
+// let para = document.getElementsByTagName("p");
+// console.dir(para);
+// console.log(para);
+
+//! Query selector
+
+let firstELe = document.querySelector("p");
+console.dir(firstELe);
+
+// let allELe = document.querySelectorAll(".cls");
+// console.dir(allELe);
+
+//? Properties
