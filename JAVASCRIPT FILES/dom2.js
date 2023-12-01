ee3f42978fc644a57656adb0fec3d9c0f9d6d2df
Akshay Umredkar <akshayumredkar9822@gmail.com>
2023-12-02 01:12:30 +0530

    create file

---
+++ JAVASCRIPT FILES/dom2.js
@@ -0,0 +1,39 @@
+// let div=document.querySelector("div");
+// console.log(div);
+
+// ? GET ATTRIBUTE
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name = div.getAttribute("name");
+// console.log(name);
+
+// let a = document.querySelector("p");
+// console.log(a.getAttribute("class"));
+
+// ? SET ATTRIBUTE
+// let G = document.querySelector("p");
+// console.log(G.setAttribute("class","newClass"));
+
+// ? Styling in js
+// let div=document.querySelector("div");
+//* console.log(div.style);
+
+// div.style.backgroundColor = "black";
+// div.style.fontSize = "30px";
+// div.innerText = "Suppu😘";
+
+//? Insert new Element/tag
+//! step 1 : first create
+//! step 2 : Add
+
+let newBtn = document.createElement("button");
+newBtn.innerText = "Click ME !";
+console.log(newBtn);
+
+
+
+
+
+
+
