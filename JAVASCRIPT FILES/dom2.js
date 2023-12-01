356eb7843ae1bd1ff8c1d824e46f9b0bd9f2ed16
Akshay Umredkar <akshayumredkar9822@gmail.com>
2023-12-02 02:05:19 +0530

    update files

--- JAVASCRIPT FILES/dom2.js
+++ JAVASCRIPT FILES/dom2.js
@@ -50,6 +50,27 @@
 //  paraGraph.remove();
 //  newHeading.remove();
 
+//? create a new btn give text,background color,textColor...insert the button as the first element inside the body tag
+
+/*let newBtn = document.createElement("button");
+newBtn.innerText = "Click me!";
+newBtn.style.backgroundColor ="red";
+newBtn.style.color ="white";
+
+document.querySelector("body").prepend(newBtn)*/
+
+
+//? Create a <p> tag in html, give it a class & some styling.Now create a new class in CSS and try to append this class to the <p> element.........Did you notice, how you overwrite the class name when you add a new one?......Solve this problem using classList.
+
+let Para = document.querySelector("p");
+//classList = sare classes ki list aa jati he
+Para.classList.add("newClass");
+
+
+
+
+
+
 
 
 
