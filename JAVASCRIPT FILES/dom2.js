d6fbeb8f12d3fe4cd8bbe9fbf27fb33746a8b6c9
Akshay Umredkar <akshayumredkar9822@gmail.com>
2023-12-02 01:38:59 +0530

    update dom2 files

--- JAVASCRIPT FILES/dom2.js
+++ JAVASCRIPT FILES/dom2.js
@@ -27,9 +27,30 @@
 //! step 1 : first create
 //! step 2 : Add
 
-let newBtn = document.createElement("button");
-newBtn.innerText = "Click ME !";
-console.log(newBtn);
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "Click !";
+// console.log(newBtn);
+
+// let div=document.querySelector("div");
+//* div.append(newBtn);
+//* div.prepend(newBtn);
+//* div.before(newBtn);
+//* div.after(newBtn);
+
+ //* let paraGraph = document.querySelector("p");
+ //! paragraph nanter mi button add karun rahilo
+ //* paraGraph.after(newBtn);
+
+ //! creating a new heading 
+//  let newHeading = document.createElement("h1");
+//  newHeading.innerHTML = "<i>Learning DOM - Document Object model</i>";
+//  document.querySelector("body").prepend(newHeading);
+
+ //! IF I HAVE TO REMOVE
+//  paraGraph.remove();
+//  newHeading.remove();
+
+
 
 
 
