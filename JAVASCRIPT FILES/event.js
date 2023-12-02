6a5d976909cea9f1dde4d2629123b668225c6ab2
Akshay Umredkar <akshayumredkar9822@gmail.com>
2023-12-02 09:35:57 +0530

    update code

---
+++ JAVASCRIPT FILES/event.js
@@ -0,0 +1,12 @@
+let btn1 = document.querySelector("#btn1");
+btn1.onclick = () =>{
+    console.log("btn1 was clicked");
+    let a =23;
+    a++;
+    console.log(a);
+}
+
+let div = document.querySelector("div");
+div.onmouseover = () =>{
+    console.log("Akshay❤️Suppu");
+}
