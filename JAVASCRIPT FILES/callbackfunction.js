8ac9f2da8023100b593d42f5617cff49499b0058
Akshay Umredkar <akshayumredkar9822@gmail.com>
2023-12-05 14:59:48 +0530

    create file

---
+++ JAVASCRIPT FILES/callbackfunction.js
@@ -0,0 +1,9 @@
+//! synchronous programming 
+let a = prompt("Enter your name:");
+let b = prompt("Enter your favorite color:");
+let c = prompt("Enter the city you were born in:");
+
+console.log("Hello " + a + "! Your favorite color is " + b + " and you were born in " + c + ".");
+
+//! callback function
+
--- JAVASCRIPT FILES/objectTrick.js
+++ JAVASCRIPT FILES/objectTrick.js
@@ -1,7 +1,35 @@
 //! destructuring 
-let arr = [3,4];
-let [a,b]=arr;
+// let arr = [3,4];
+// let [a,b]=arr;
+// console.log(arr);
 
+// let arr = [3,5,8,9,12,14,18,20];
+// let [a, b, c, d, ...rest] = arr
+// console.log(a, b, c, d, rest);
 
+//? rest value i need from 12
+// let arr = [3,5,8,9,12,14,18,20];
+// let [a, , , , ...rest] = arr
+// console.log(a, rest);
 
-console.log(arr);
+// let {a,b} = {a:1,b:9}
+// console.log(a,b);
+
+//! Spread Operator
+let arr1 = [3,6,9]
+let obj1 = {...arr1}
+console.log(obj1);
+
+const sum = (v1, v2, v3) => {
+    return v1+v2+v3
+};
+
+console.log(sum(...arr1));
+
+let obj2 = {
+    name:"Akshay",
+    company:"Microsoft",
+    address: "Bangalore"
+}
+// console.log({company:"Google",...obj2}); //this will print the obj2 object without changing any value
+console.log({...obj2,company:"Google"});
