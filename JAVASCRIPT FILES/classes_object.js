52648207e1da63d41746fcc4217c4dc7ae88178d
Akshay Umredkar <akshayumredkar9822@gmail.com>
2023-12-11 19:47:37 +0530

    update

--- JAVASCRIPT FILES/classes_object.js
+++ JAVASCRIPT FILES/classes_object.js
@@ -8,12 +8,46 @@
 //     },
 // };
 
-const employees = {
-    calTax() {
-        console.log('tax rate is 10%')
-    }
-}
-const karanArjun = {
-    salary : 50000,      
-}
-karanArjun.__proto__ = employees;
+//! Objects
+
+// const employees = {
+//   //? employees = objects
+//   calTax() {
+//     //?calTax = function
+//     console.log("tax rate is 10%");
+//   },
+//   calTax2: function () {
+//     console.log("tax rate is 50% ");
+//   },
+// };
+
+// const karanArjun = {
+//     salary: 50000,
+//     calTax3() {
+//         console.log('tax rate is 20%');
+//     }}
+// karanArjun.__proto__ = employees;
+
+//! classes
+// class tata {
+//   constructor() {
+//     this.brandName = "NexonEC";
+//   }
+
+//   start() {
+//     console.log("start");
+//   }
+
+//   stop() {
+//     console.log("stop");
+//   }
+
+//   setBrand(brand) {
+//     //? brand = argument
+//     this.brandName = brand; //?Instance Method setBrand
+//     //? this refers to the current instance of the class
+//   }
+// }
+
+// let nexon = new tata();
+// nexon.setBrand("E4 Model");
